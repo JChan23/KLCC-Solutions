61f123ff43169501ee69b1b77e926ad63db4716e
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-14 22:50:42 +0800

    Update TestCase2.py

--- Bronze/Question 3/TestCase2.py
+++ Bronze/Question 3/TestCase2.py
@@ -1,5 +1,6 @@
 dictionary = {}
 string = "lLq5KWgbpTxHYMD7IZ6IMn3MHuFrurmjpRGa8CL14EtnFIiIjBeIDIQQ01c2GOQbm1jDoeMiT7xwYkqpptg7zmGH16FoLa1PMg1GAk6kWXNKTRNH4DlMwKYwMWUggxOfRE2ZAERfDRTpCv4WcKXdqV94jYm9IdrVkaQSqjbvhVtfAZBp4G7TcmCUMbwjKRaKMS6d6zW8"
+
 for i in range(len(string)):
     if string[i] not in dictionary:
         dictionary[string[i]] = 1
