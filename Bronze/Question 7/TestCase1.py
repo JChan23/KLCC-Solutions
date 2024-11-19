d5be68e832f1f80a94b09177f23fb1a64781d864
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-19 22:36:31 +0800

    Update TestCase1.py

--- Bronze/Question 7/TestCase1.py
+++ Bronze/Question 7/TestCase1.py
@@ -1,8 +1,4 @@
-array = [22, 35, 5, -7, -25, -2, 31, -40, -33, -1, 36, -18, -11, 33, -47, 48, -20, -24, 41, -43, 8, 9, 12, 14, 50]
-sum = 0
+str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
 
-for i in range(len(array)):
-    if array[i] % 2 == 0:
-        sum = sum + array[i]
-
-print(sum)
+reverse = str[::-1]
+print(reverse)
