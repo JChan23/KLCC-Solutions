d753ae105c4f7982996e4a9691ae06cc6f016403
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-19 22:49:49 +0800

    Update TestCase1.py

--- Bronze/Question 8/TestCase1.py
+++ Bronze/Question 8/TestCase1.py
@@ -1,10 +1,12 @@
-array = [1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 3, 9, 10, 4, 11, 12, 6, 13, 14, 15, 8, 16, 17, 18, 9, 19, 20]
-no_duplicates = []
+import string
 
-for i in range(len(array)):
-    if array[i] not in no_duplicates:
-        no_duplicates.append(array[i])
+original = "Kuala Lumpur, officially the Federal Territory of Kuala Lumpur, and colloquially referred to as KL, is the capital city and a federal territory of Malaysia. It is the largest city in the country, covering an area of 243 km^2 with a census population of two million as of 2024. Greater Kuala Lumpur, also known as the Klang Valley, is an urban agglomeration of nine million people as of 2024. It is among the fastest growing metropolitan regions in Southeast Asia, both in population and economic development."
+no_punctuation = original.translate(str.maketrans('', '', string.punctuation)) #Removes punctuation from paragraph
+paragraph = no_punctuation.split(" ") #stores erch individual word seperated by a space in a list
 
-print(no_duplicates)
+longest_word = ""
+for i in range(len(paragraph)):
+    if len(paragraph[i]) > len(longest_word):
+        longest_word = paragraph[i]
 
-# The "list(set(array))" method will also work for this test case, as the elements of the original list are ordered
+print(longest_word)
