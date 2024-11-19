b615502c3697c9824776f02f02b88752570e3770
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-19 23:00:08 +0800

    Update TestCase2.py

--- Bronze/Question 9/TestCase2.py
+++ Bronze/Question 9/TestCase2.py
@@ -1,4 +1,6 @@
-str = "The quick brown fox jumps over the lazy dog several times in a row just to see if the dog could catch up"
+n = 999
 
-reverse = str[::-1]
-print(reverse)
+if n % 2 == 1:
+    print(((n+3)/2)+(n**2))
+else:
+    print(((n+4)/2)+(0.25*(n**2)))
