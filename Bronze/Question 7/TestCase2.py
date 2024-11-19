ba159e29ea051069e4567031c0a559dbd966b9d8
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-19 22:36:46 +0800

    Update TestCase2.py

--- Bronze/Question 7/TestCase2.py
+++ Bronze/Question 7/TestCase2.py
@@ -1,8 +1,4 @@
-array = [487, -631, 902, -759, 123, -214, 730, -812, 564, 291, -148, 456, 832, -925, 687, -451, 320, -117, 794, 921, -329, 604, -938, 481, -272, 110, 387, -659, 829, -502, 705, -799, 173, 258]
-sum = 0
+str = "The quick brown fox jumps over the lazy dog several times in a row just to see if the dog could catch up"
 
-for i in range(len(array)):
-    if array[i] % 2 == 0:
-        sum = sum + array[i]
-
-print(sum)
+reverse = str[::-1]
+print(reverse)
