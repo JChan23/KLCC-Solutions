48a74787e78b5712779bd93ef508873ff248115d
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-19 22:20:53 +0800

    Create TestCase1.py

---
+++ Bronze/Question 3/TestCase1.py
@@ -0,0 +1,8 @@
+array = [10, 15, 2, 15, 11, 2, 14, 18, 15, 15, 10, 2, 16, 8, 17, 11, 11, 8, 19, 17]
+count = 0
+
+for i in range(len(array)):
+    if array[i] % 11 == 0:
+        count = count + 1
+
+print(count)
