7b5c6a9c40b61fcf09723fb1f1b4e8098d19998c
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-21 23:11:36 +0800

    Update TestCase1.py

--- Silver/Question 4/TestCase1.py
+++ Silver/Question 4/TestCase1.py
@@ -4,9 +4,11 @@ most_0s = 0
 for index, row in enumerate(dungeon):
     consecutive_0s = 0
     for num in row:
-        if num:
+        if num == 0:
             consecutive_0s = consecutive_0s + 1
             if consecutive_0s > most_0s:
                 most_0s = consecutive_0s
+        else:
+            consecutive_0s = 0
 
 print(most_0s)
