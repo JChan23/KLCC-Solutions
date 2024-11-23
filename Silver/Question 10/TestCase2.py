ba323e43fceceeb60d6117485676df62fb93a1df
JChan23 <143363241+JChan23@users.noreply.github.com>
2024-11-23 15:22:03 +0800

    Create TestCase2.py

---
+++ Silver/Question 10/TestCase2.py
@@ -0,0 +1,14 @@
+Start = 100
+End = 2810
+
+harshad = []
+
+for i in range(Start, End+1):
+    total = 0
+    num = str(i)
+    for j in range(len(num)):
+        total = total + int(num[j])
+    if (i/total) % 1 == 0:
+        harshad.append(i)
+
+print(sum(harshad))
